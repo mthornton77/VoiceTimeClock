7d72dc5a4fdc844aad145dcb68e43b079521bb69
mike <mike@dell>
2012-09-17 01:10:34 -0400

    added db

--- src/edu/cmu/pocketsphinx/demo/PunchClass.java
+++ src/edu/cmu/pocketsphinx/demo/PunchClass.java
@@ -12,20 +12,21 @@ public class PunchClass {
 
 	public static String getPunchTime() {
 		DateFormat dateFormat = new SimpleDateFormat("MM/dd/yyyy HH:mm");
-		// get current date time with Date()
-		Date date = new Date();
 		Calendar calendar = Calendar.getInstance();
-		calendar.setTime(date);
+		// get minute of "now"
+		calendar.setTime(new Date());
 		int min = calendar.get(Calendar.MINUTE);
-		
-		 int mod=min%30;
-		    if(mod>30/2){
-		     min=30-mod;
-		    }else{
-		     min=0-mod;
-		    }
-		    calendar.add(Calendar.MINUTE, min);
+		// round to nearest 30min
+		int mod = min % 30;
+		if (mod > 30 / 2) {
+			min = 30 - mod;
+		} 
+		else {
+			min = 0 - mod;
+		}
+		calendar.add(Calendar.MINUTE, min);
 		punchTime = (dateFormat.format(calendar.getTime()));
+
 		return punchTime;
 	}
 
